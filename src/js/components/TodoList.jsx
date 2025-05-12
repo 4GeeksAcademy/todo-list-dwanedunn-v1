423a9290291e3ca586a04cc149c76026de3bc7f2
Dwane Dunn <dwane.dunn@gmail.com>
2025-05-12 23:45:44 +0000

    add todo count

--- src/js/components/TodoList.jsx
+++ src/js/components/TodoList.jsx
@@ -3,17 +3,24 @@ import TodoItem from './TodoItem';
 
 const TodoList = ({ todos, handleToggleComplete, handleDeleteTodo }) => {
   return (
-    <ul className="todo-list">
-      {todos.map((todo, index) => (
-        <TodoItem
-          key={index}
-          todo={todo}
-          index={index}
-          handleToggleComplete={handleToggleComplete}
-          handleDeleteTodo={handleDeleteTodo}
-        />
-      ))}
-    </ul>
+    <>
+      <ul className="todo-list">
+        {todos.map((todo, index) => (
+          <TodoItem
+            key={index}
+            todo={todo}
+            index={index}
+            handleToggleComplete={handleToggleComplete}
+            handleDeleteTodo={handleDeleteTodo}
+          />
+        ))}
+      </ul>
+      <div className="footer">
+        <p className="todo-count">
+          {todos.length} {todos.length === 1 ? 'item' : 'items'} left
+        </p>
+      </div>
+    </>
   );
 };
 
